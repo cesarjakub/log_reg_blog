3c274ec1da93b4dd9f3e115ea605a1603e5655ed
Jakub César <84319967+cesarjakub@users.noreply.github.com>
2023-10-25 16:12:42 +0200

    feat: Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ import serveIndex from 'serve-index';
 import path from 'path';
 import { URL } from 'url';
 import { fileURLToPath } from 'url';
-
+                                          
 const app = express();
 
 const __filename = fileURLToPath(import.meta.url);
@@ -16,23 +16,45 @@ app.use((err, req, res, next) => {
 
 app.use(express.json());
 
-app.use('/public', express.static('public'));
-app.use('/public', serveIndex('public'));
-
 app.get('/', (req, res) => {
   res.redirect("/index.html");
 });
 
 app.get("/public", (req, res) => {
-  res.redirect("/index.html")
+  //res.sendFile(path.join(__dirname+"/index.html"));
+  res.redirect("/index.html");
 });
 
 app.get("/index.html", (req, res) => {
         res.sendFile(path.join(__dirname+"/index.html"));
 });
 
+app.get("/public", (req, res) => {
+  res.redirect("/index.html")
+});
+
 app.get("/register.html", (req, res) => {
         res.sendFile(path.join(__dirname+"/register.html"));
 });
 
+/*
+app.route("/register.html")
+        .get((req, res) => {
+                res.sendFile(path.join(__dirname + "/register.html"));
+        })
+        .post(async (req, res) => {
+                const { Lname, Lpass } = req.body;
+        const user = await register(Lname, Lpass);
+        res.status(201);
+        // redirect to main page
+                console.log(req.body);
+        });
+*/
+
+app.get("/blog.html", (req, res) => {
+        res.sendFile(path.join(__dirname+"/blog.html"));
+});
+
+app.use('/public', express.static('public'));
+app.use('/public', serveIndex('public'));
 app.listen(3000, () => console.log('Example app is listening on port 3000.'));
